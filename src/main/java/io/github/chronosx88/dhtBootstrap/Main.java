9179cf6fb32415a46f147332e796585e2f5ad35d
ChronosX88 <chronosx88@gmail.com>
2019-03-05 15:24:33 +0400

    Added TomP2P to dependency.

--- src/main/java/io/github/chronosx88/dhtBootstrap/Main.java
+++ src/main/java/io/github/chronosx88/dhtBootstrap/Main.java
@@ -1,16 +1,12 @@
 package io.github.chronosx88.dhtBootstrap;
 
-import io.github.chronosx88.kademliadht.DefaultConfiguration;
-import io.github.chronosx88.kademliadht.JKademliaNode;
-import io.github.chronosx88.kademliadht.node.KademliaId;
-import io.github.chronosx88.kademliadht.node.Node;
-
 import java.io.IOException;
 import java.net.Inet4Address;
 
 public class Main {
-    private static JKademliaNode node;
+    //private static JKademliaNode node;
     public static void main(String[] args) {
+        /*
         try {
             KademliaId kadID = new KademliaId("sgCZ+fg49g4N8FU43kW84cNVPTw=");
             node = new JKademliaNode("Main Bootstrap Node", new Node(kadID, Inet4Address.getLocalHost(), 7243), 7243, new DefaultConfiguration());
@@ -18,5 +14,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
     }
 }
